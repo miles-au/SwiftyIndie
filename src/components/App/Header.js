ce1c47ca503266de3f9b2753699aee593b2ef34d
Miles Au <miles.d.au@gmail.com>
2024-09-12 19:32:48 +0200

    Remove header logo

--- src/components/App/Header.js
+++ src/components/App/Header.js
@@ -6,8 +6,8 @@ const Header = ({ isLoggedIn }) => {
     return (
         <header>
             <div className="logo">
-                    <img className="logo-icon" src={require('./logo.png')} alt='Before You Go Logo' />
-                    <h1 className="logo-text">BeforeYouGo</h1>
+                    {/* <img className="logo-icon" src={require('./logo.png')} alt='Before You Go Logo' /> */}
+                    <h1 className="logo-text">SwiftyIndie</h1>
                 </div>
         </header>
     );
