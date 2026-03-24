75cdadd7d22fa84b8271b42004aa50b66949a18a
Miles Au <miles.d.au@gmail.com>
2026-03-24 01:06:26 +0100

    Link header title to the home page

--- src/components/App/Header.js
+++ src/components/App/Header.js
@@ -4,9 +4,11 @@ const Header = ({ isLoggedIn }) => {
     return (
         <header>
             <div className="logo">
+                <a className="logo-link" href="#/">
                     {/* <img className="logo-icon" src={require('./logo.png')} alt='Before You Go Logo' /> */}
                     <h1 className="logo-text">SwiftyIndie</h1>
-                </div>
+                </a>
+            </div>
         </header>
     );
 }
